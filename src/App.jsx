18ce9fb455cefe212c05f26262e197e4b219a944
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2024-02-27 20:15:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-
 function App() {
   const [uuid, setUuid] = useState(crypto.randomUUID());
   const [message, setMessage] = useState("");
